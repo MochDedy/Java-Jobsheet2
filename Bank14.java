152c44f8af5a16ca2486d159215996042120e2c6
MochDedy <mochdedy96@gmail.com>
2025-09-27 21:44:59 +0700

    first commit

---
+++ Bank14.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bank14 {
+    public static void main (String [] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int jml_tabungan_awal, lama_menabung;
+        double jml_tabungan_akhir, bunga, prosentase_bunga = 0.02;
+
+        System.out.print("Masukkan jumlah tabungan awal anda: ");
+        jml_tabungan_awal = input.nextInt();
+
+        System.out.print("Masukkan lama menabung anda: ");
+        lama_menabung = input.nextInt();
+
+        bunga = lama_menabung*prosentase_bunga*jml_tabungan_awal;
+        jml_tabungan_akhir = bunga + jml_tabungan_awal;
+
+        System.out.println("Bunga adalah " +(int) bunga);
+        System.out.println("Jumlah tabungan akhir anda adalah " +(int) jml_tabungan_akhir);
+    }
+    
+}
